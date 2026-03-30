ddeb9028b2fb8eea7826100869788bd09a7398bd
skempfer <shana.kempfer88@gmail.com>
2026-03-30 10:07:13 -0300

    Add exercise 8.19

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,13 +1,24 @@
+import { useState } from 'react'
+
 const Books = (props) => {
+  const [selectedGenre, setSelectedGenre] = useState('all genres')
+
   if (!props.show) {
     return null
   }
 
   const books = props.books
+  const genres = [...new Set(books.flatMap((book) => book.genres))]
+  const filteredBooks =
+    selectedGenre === 'all genres'
+      ? books
+      : books.filter((book) => book.genres.includes(selectedGenre))
 
   return (
     <div>
       <h2>books</h2>
+
+      {selectedGenre === 'all genres' ? null : <div>in genre {selectedGenre}</div>}
 
       <table>
         <tbody>
@@ -16,7 +27,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {filteredBooks.map((a) => (
             <tr key={a.id}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -25,6 +36,15 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+
+      <div>
+        {genres.map((genre) => (
+          <button key={genre} onClick={() => setSelectedGenre(genre)}>
+            {genre}
+          </button>
+        ))}
+        <button onClick={() => setSelectedGenre('all genres')}>all genres</button>
+      </div>
     </div>
   )
 }
--- src/queries.js
+++ src/queries.js
@@ -16,6 +16,7 @@ export const ALL_BOOKS = gql`
       id
       title
       published
+      genres
       author {
         name
       }
